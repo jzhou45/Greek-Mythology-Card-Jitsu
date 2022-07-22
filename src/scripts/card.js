71461e0320da923d44c1fce024313e6fe4e8be38
jzhou45 <jonathanzhou77@gmail.com>
2022-07-22 13:44:47 -0400

    added to card class

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,7 +1,8 @@
-function Card(name, type, value, url){
+function Card(name, type, value, color, url){
     this.name = name;
     this.type = type;
     this.value = value;
+    this.color = color;
     this.img = url;
 }
 
