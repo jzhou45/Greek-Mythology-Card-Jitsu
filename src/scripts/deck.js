e850f61d98c27ed0b3581d751b4701fd9a2230d6
jzhou45 <jonathanzhou77@gmail.com>
2022-07-22 14:03:56 -0400

    added shuffle to deck

--- src/scripts/deck.js
+++ src/scripts/deck.js
@@ -1,3 +1,11 @@
 function Deck(...cards){
     this.cards = cards;
 }
+
+Deck.prototype.shuffle = function(){
+    for (let i = 0; i < this.cards.length; i++){
+        const randomIndex = i + Math.floor(Math.random() * this.cards.length % (this.cards.length - i));
+        [this.cards[i], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[i]];
+ }
+    return this.cards;
+}
