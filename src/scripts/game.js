72889ea83bd4b24da084af94fdd6e077af0528f7
jzhou45 <jonathanzhou77@gmail.com>
2022-07-23 15:16:22 -0400

    allowed for cards to be shuffled and drawn randomly

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,23 +2,40 @@ import Deck from "./deck.js";
 import Hand from "./hand.js";
 
 function Game(deck){
-    this.hand = new Hand(deck);
-    this.deck = this.hand.deck;
+    let hand = new Hand(deck);
+    this.handClass = hand;
+    this.hand = hand.hand;
+    this.deck = hand.deck;
 }
 
 Game.prototype.start = function(){
     this.deck.shuffle();
-    this.hand.draw()
+    this.initialDraw();
 }
 
 Game.prototype.initialDraw = function(){
-    let card1 = this.hand[0];
-    let card2 = this.hand[1];
-    let card3 = this.hand[2];
-    let card4 = this.hand[3];
-    let card5 = this.hand[4];
+    this.handClass.initialDraw();
+    const card1 = {card: this.hand[0], id: "card1"};
+    const card2 = {card: this.hand[1], id: "card2"};
+    const card3 = {card: this.hand[2], id: "card3"};
+    const card4 = {card: this.hand[3], id: "card4"};
+    const card5 = {card: this.hand[4], id: "card5"};
 
-    console.log(card1.children);
+    this.setCardValues(card1);
+    this.setCardValues(card2);
+    this.setCardValues(card3);
+    this.setCardValues(card4);
+    this.setCardValues(card5);
+}
+
+Game.prototype.setCardValues = function(cardObj){
+    let backgroundColor = document.getElementById(cardObj.id);
+    let upperCorner = backgroundColor.children[0];
+    let img = backgroundColor.children[1];
+
+    backgroundColor.style.backgroundColor = cardObj.card.color;
+    upperCorner.style.backgroundColor = cardObj.card.color;
+    img.src = cardObj.card.img;
 }
 
 export default Game;
