72cfc71e0962441d329c50ded535164cf17ada4e
jzhou45 <jonathanzhou77@gmail.com>
2022-07-24 00:22:24 -0400

    added Poseidon pack

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import Game from "./scripts/game.js";
 import Board from "./scripts/board.js"
 
 document.addEventListener('DOMContentLoaded', () => {
-    let zeus = new Card("Zeus", "god", "10", "#FDFD96", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8932fa02-b339-4e8c-99d6-115e7cd29415/d1qn6k2-29220961-dc57-459b-ad1a-4015d7e6067b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg5MzJmYTAyLWIzMzktNGU4Yy05OWQ2LTExNWU3Y2QyOTQxNVwvZDFxbjZrMi0yOTIyMDk2MS1kYzU3LTQ1OWItYWQxYS00MDE1ZDdlNjA2N2IuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Q5fKUiaddvfZlaLo42gTYWA0YbXxB-UahlYmE3xwSLI");
-    let hera = new Card("Hera", "god", "1", "#F8C8DC", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/73fd5feb-8a2e-46e3-8ab9-c764280fb325/d94v10j-7630529f-92fa-4db5-be45-4b1666e7072a.jpg/v1/crop/w_167,h_250,x_0,y_0,scl_0.4175,q_70,strp/hera_by_ninjart1st_d94v10j-250t.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjAwIiwicGF0aCI6IlwvZlwvNzNmZDVmZWItOGEyZS00NmUzLThhYjktYzc2NDI4MGZiMzI1XC9kOTR2MTBqLTc2MzA1MjlmLTkyZmEtNGRiNS1iZTQ1LTRiMTY2NmU3MDcyYS5qcGciLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.zRy2xWQLZ4e8ahj-vXwgp5yPj7o8YKGberRXYpBKKEw");
-    let dionysus = new Card("Dionysus", "god", "2", "#C3B1E1", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b65a42f8-65e1-488a-b310-5a5137f199f3/d5cgbr8-1e89258e-2661-4c56-bba3-0842e271a463.jpg/v1/fill/w_400,h_553,q_75,strp/dionysus_by_reapersun_d5cgbr8-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I2NWE0MmY4LTY1ZTEtNDg4YS1iMzEwLTVhNTEzN2YxOTlmM1wvZDVjZ2JyOC0xZTg5MjU4ZS0yNjYxLTRjNTYtYmJhMy0wODQyZTI3MWE0NjMuanBnIiwiaGVpZ2h0IjoiPD01NTMiLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd21cL2I2NWE0MmY4LTY1ZTEtNDg4YS1iMzEwLTVhNTEzN2YxOTlmM1wvcmVhcGVyc3VuLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.eEm06H8MgeotM1_KJOo6xETrikoR7dmEsFyetBS20Rg");
-    let theseus = new Card("Theseus", "hero", "7", "#A7C7E7", "https://www.greek-gods.info/greek-heroes/theseus/images/theseus-painting-gallery.jpg");
-    let medusa = new Card("Medusa", "monster", "9", "#A7C7E7", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/241f2636-882f-4357-9e99-b9a3cbcfa999/ddkv05n-6105a9aa-faa6-47cc-8ab9-33a7e79ff5df.jpg/v1/fill/w_1024,h_1353,q_75,strp/medusa_by_z__ed_ddkv05n-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTM1MyIsInBhdGgiOiJcL2ZcLzI0MWYyNjM2LTg4MmYtNDM1Ny05ZTk5LWI5YTNjYmNmYTk5OVwvZGRrdjA1bi02MTA1YTlhYS1mYWE2LTQ3Y2MtOGFiOS0zM2E3ZTc5ZmY1ZGYuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.PpPED2cXuDG_OHzYB2UBnFA0-HZ7NNHMV6FP4D_SQ5U");
+    const poseidon = new Card("Poseidon", "god", "9", "#A7C7E7", "/src/assests/poseidon.png");
+    const triton = new Card("Triton", "god", "5", "#A7C7E7", "/src/assests/triton.png");
+    const proteus = new Card("Proteus", "god", "1", "#A7C7E7", "src/assests/proteus.jpeg");
+    const orion = new Card("Orion", "hero", "3", "#A7C7E7", "/src/assests/orion.jpeg");
+    const theseus = new Card("Theseus", "hero", "7", "#A7C7E7", "/src/assests/theseus.webp");
+    const bellerophon = new Card("Bellerophon", "hero", "2", "#A7C7E7", "/src/assests/bellerophon.jpeg");
+    const minotaur = new Card("Minotaur", "monster", "9", "#A7C7E7", "/src/assests/minotaur.png");
+    const pegasus = new Card("Pegasus", "monster", "10", "#A7C7E7", "/src/assests/pegasus.png");
+    const arion = new Card("Arion", "monster", "1", "#A7C7E7", "/src/assests/arion.jpeg");
     
-    var starterDeck = new Deck(zeus, hera, dionysus, theseus, medusa);
+    var starterDeck = new Deck(poseidon, orion, theseus, bellerophon, triton, proteus, minotaur, pegasus, arion);
 
     var game = new Game(starterDeck);
     game.start();
