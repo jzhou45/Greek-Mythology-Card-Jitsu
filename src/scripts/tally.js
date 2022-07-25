8743bee4d87bffbd16945567ce7252b2461f30b1
jzhou45 <jonathanzhou77@gmail.com>
2022-07-25 14:13:35 -0400

    added helper functions for points

--- src/scripts/tally.js
+++ src/scripts/tally.js
@@ -3,3 +3,17 @@ function Tally(player){
     this.player = player;
 }
 
+Tally.prototype.type = function(i){
+    return this.points[i].type;
+}
+
+Tally.prototype.color = function(i){
+    return this.points[i].color;
+}
+
+// Tally.prototype.winGame = function(){
+//     let points = {"god": [], "monster": [], "hero": [],};
+//     for (let i = 0; i < this.points.length; i++){
+//         if this.
+//     }
+// }
