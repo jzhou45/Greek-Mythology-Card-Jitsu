face73654ac447982c06a3c22cd698ce47ee9c22
jzhou45 <jonathanzhou77@gmail.com>
2022-07-26 12:03:21 -0400

    added alternative win condition

--- src/scripts/tally.js
+++ src/scripts/tally.js
@@ -11,12 +11,9 @@ Tally.prototype.color = function(i){
     return this.points[i].color;
 }
 
-Tally.prototype.winTypes = function(i){
+Tally.prototype.winTypes = function(){
     let points = {"monster": 0, "god": 0, "hero": 0};
     for (let i of points){
-        if (points.monster >= 1 && points.god >= 1 && points.hero >= 1){
-            return this.player;
-        }
         if (i.type === "monster"){
             points["monster"]++;
         } else if (i.type === "god"){
@@ -24,6 +21,27 @@ Tally.prototype.winTypes = function(i){
         } else {
             points["hero"]++
         }
+        if (points.monster >= 1 && points.god >= 1 && points.hero >= 1){
+            return this.player;
+        }
     }
     return false;
 }
+
+Tally.prototype.winColors = function(){
+    let points = {"monster": [], "god": [], "hero": []};
+    for (let i of points){
+        if (i.type === "monster" && !points.monster.includes(i.color)){
+            points["monster"].push(i.color);
+        } else if (i.type === "hero" && !points.hero.includes(i.color)){
+            points["hero"].push(i.color);
+        } else if (i.type === "god" && !points.god.includes(i.color)){
+            points["god"].push(i.color);
+        }
+        if (points.monster >= 1 && points.god >= 1 && points.hero >= 1){
+            return this.player;
+        }
+    }
+    return false;
+}
+
