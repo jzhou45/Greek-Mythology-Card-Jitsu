e1f0333d5ea8b2e4ef99bab743afe3f2c91f4338
jzhou45 <jonathanzhou77@gmail.com>
2022-07-26 11:49:57 -0400

    added function to check wins

--- src/scripts/tally.js
+++ src/scripts/tally.js
@@ -11,9 +11,19 @@ Tally.prototype.color = function(i){
     return this.points[i].color;
 }
 
-// Tally.prototype.winGame = function(){
-//     let points = {"god": [], "monster": [], "hero": [],};
-//     for (let i = 0; i < this.points.length; i++){
-//         if this.
-//     }
-// }
+Tally.prototype.winTypes = function(i){
+    let points = {"monster": 0, "god": 0, "hero": 0};
+    for (let i of points){
+        if (points.monster >= 1 && points.god >= 1 && points.hero >= 1){
+            return this.player;
+        }
+        if (i.type === "monster"){
+            points["monster"]++;
+        } else if (i.type === "god"){
+            points["god"]++;
+        } else {
+            points["hero"]++
+        }
+    }
+    return false;
+}
