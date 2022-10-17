1a3eca87534c12eba195162c237a75e93dffd4af
jzhou45 <jonathanzhou77@gmail.com>
2022-10-17 18:36:57 -0400

    refactor ai to es6

--- src/scripts/ai.js
+++ src/scripts/ai.js
@@ -1,53 +1,55 @@
 import Deck from "./deck.js"
 
-function AI(){
-    this.deck = new Deck();
-    this.board = null;
-}
+class AI{
+    constructor(){
+        this.deck = new Deck();
+        this.board = null;
+    };
 
-AI.prototype.play = function(){
-    this.board = this.deck.draw();
-    this.moveCardToBoard();
-}
+    play(){
+        this.board = this.deck.draw();
+        this.moveCardToBoard();
+    };
 
-AI.prototype.clear = function(){
-    this.board = null;
-    this.undisplayCardName("ai-board-name");
-    const board = document.getElementById("ai-board");
-    board.style.display = "none";
-}
+    clear(){
+        this.board = null;
+        this.undisplayCardName("ai-board-name");
+        const board = document.getElementById("ai-board");
+        board.style.display = "none";
+    };
 
-AI.prototype.moveCardToBoard = function(){
-    let board = document.getElementById("ai-board")
-    let upperCorner = board.children[0];
-    let img = board.children[1];
-    let icon = upperCorner.children[0];
-    let powerValue = upperCorner.children[1];
+    moveCardToBoard(){
+        let board = document.getElementById("ai-board")
+        let upperCorner = board.children[0];
+        let img = board.children[1];
+        let icon = upperCorner.children[0];
+        let powerValue = upperCorner.children[1];
+    
+        board.style.display = "flex";
+        board.style.backgroundColor = this.board.color;
+        upperCorner.style.backgroundColor = this.board.color;
+        img.src = this.board.img;
+        if (this.board.type === "god"){
+            icon.src = "src/assets/god.png";
+        } else if (this.board.type === "monster"){
+            icon.src = "src/assets/monster.png";
+        } else{
+            icon.src = "src/assets/hero.png"
+        };
+    
+        powerValue.innerHTML = this.board.value;
+        this.displayCardName("ai-board-name");
+    };
 
-    board.style.display = "flex";
-    board.style.backgroundColor = this.board.color;
-    upperCorner.style.backgroundColor = this.board.color;
-    img.src = this.board.img;
-    if (this.board.type === "god"){
-        icon.src = "src/assets/god.png";
-    } else if (this.board.type === "monster"){
-        icon.src = "src/assets/monster.png";
-    } else{
-        icon.src = "src/assets/hero.png"
-    }
+    displayCardName(divElement){
+        const div = document.getElementById(divElement);
+        div.children[0].innerHTML = this.board.name;
+    };
 
-    powerValue.innerHTML = this.board.value;
-    this.displayCardName("ai-board-name");
-}
-
-AI.prototype.displayCardName = function(divElement){
-    const div = document.getElementById(divElement);
-    div.children[0].innerHTML = this.board.name;
-}
-
-AI.prototype.undisplayCardName = function(divElement){
-    const div = document.getElementById(divElement);
-    div.children[0].innerHTML = "";
-}
+    undisplayCardName(divElement){
+        const div = document.getElementById(divElement);
+        div.children[0].innerHTML = "";
+    };
+};
 
 export default AI;
