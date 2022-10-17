728539ed2ef9c14ba81d282fe6f0cfac574ec1ce
jzhou45 <jonathanzhou77@gmail.com>
2022-10-17 18:47:24 -0400

    refactor hand to es6

--- src/scripts/hand.js
+++ src/scripts/hand.js
@@ -1,95 +1,75 @@
 import Deck from "./deck.js";
 
-function Hand(){
-    this.hand = [null, null, null, null, null];
-    this.deck = new Deck();
-    this.initialDraw()
-}
+class Hand{
+    constructor(){
+        this.hand = [null, null, null, null, null];
+        this.deck = new Deck();
+        this.initialDraw();
+    };
 
-Hand.prototype.initialDraw = function(){
-    for (let i = 0; i < 5; i++){
-        this.hand[i] = this.draw();
-    }
-    return this.hand;
-}
+    initialDraw(){
+        for (let i = 0; i < 5; i++){
+            this.hand[i] = this.draw();
+        };
+        return this.hand;
+    };
 
-Hand.prototype.showDraw = function(index){
-    let card;
-    if (index === 0){
-        card = {card: this.hand[0], id: "card1"};
-    } else if (index === 1){
-        card = {card: this.hand[1], id: "card2"};
-    } else if (index === 2){
-        card = {card: this.hand[2], id: "card3"};
-    } else if (index === 3){
-        card = {card: this.hand[3], id: "card4"};
-    } else {
-        card = {card: this.hand[4], id: "card5"};
-    }
-    this.setCardValues(card);
-}
+    showDraw(index){
+        let card = {card: this.hand[index], id: `card${index + 1}`}
+        this.setCardValues(card);
+    };
 
-Hand.prototype.setCardValues = function(cardObj){
-    let backgroundColor = document.getElementById(cardObj.id);
-    let upperCorner = backgroundColor.children[0];
-    let img = backgroundColor.children[1];
-    let icon = upperCorner.children[0];
-    let powerValue = upperCorner.children[1];
+    setCardValues(cardObj){
+        let backgroundColor = document.getElementById(cardObj.id);
+        let upperCorner = backgroundColor.children[0];
+        let img = backgroundColor.children[1];
+        let icon = upperCorner.children[0];
+        let powerValue = upperCorner.children[1];
+    
+        backgroundColor.style.backgroundColor = cardObj.card.color;
+        upperCorner.style.backgroundColor = cardObj.card.color;
+        img.src = cardObj.card.img;
+        if (cardObj.card.type === "god"){
+            icon.src = "src/assets/god.png";
+        } else if (cardObj.card.type === "monster"){
+            icon.src = "src/assets/monster.png";
+        } else{
+            icon.src = "src/assets/hero.png"
+        };
+    
+        powerValue.innerHTML = cardObj.card.value;
+    };
 
-    backgroundColor.style.backgroundColor = cardObj.card.color;
-    upperCorner.style.backgroundColor = cardObj.card.color;
-    img.src = cardObj.card.img;
-    if (cardObj.card.type === "god"){
-        icon.src = "src/assets/god.png";
-    } else if (cardObj.card.type === "monster"){
-        icon.src = "src/assets/monster.png";
-    } else{
-        icon.src = "src/assets/hero.png"
-    }
+    draw(){
+        const newCard = this.deck.draw();
+        const index = this.findMissingCardIndex();
+        this.hand[index] = newCard;
+        this.showDraw(index);
+        return newCard;
+    };
 
-    powerValue.innerHTML = cardObj.card.value;
-}
+    playCard(index){
+        const playedCard = this.hand[index];
+        this.hand[index] = null;
+        return playedCard;
+    };
 
-Hand.prototype.draw = function(){
-    const newCard = this.deck.draw();
-    const index = this.findMissingCardIndex();
-    this.hand[index] = newCard;
-    this.showDraw(index)
-    return newCard;
-}
+    isMissingCard(){
+        for (let i of this.hand){
+            if (i === null) return false;
+        };
+        return true;
+    };
 
+    findMissingCardIndex(){
+        for (let i = 0; i < this.hand.length; i++){
+            if (this.hand[i] === null) return i;
+        };
+    };
 
-Hand.prototype.playCard = function(index){
-    const playedCard = this.hand[index];
-    this.hand[index] = null;
-    return playedCard;
-}
-
-Hand.prototype.isMissingCard = function(){
-    for (let i of this.hand){
-        if (i === null) return false;
-    }
-    return true;
-}
-
-Hand.prototype.findMissingCardIndex = function(){
-    for (let i = 0; i < this.hand.length; i++){
-        if (this.hand[i] === null) return i;
-    }
-}
-
-Hand.prototype.whichCard = function(element){
-    if (element === "card1"){
-        return this.hand[0];
-    } else if (element === "card2"){
-        return this.hand[1];
-    } else if (element === "card3"){
-        return this.hand[2];
-    } else if (element === "card4"){
-        return this.hand[3];
-    } else{
-        return this.hand[4];
-    }
-}
+    whichCard(element){
+        return this.hand[parseInt(element[-1]) - 1]
+    };
+};
 
 export default Hand;
